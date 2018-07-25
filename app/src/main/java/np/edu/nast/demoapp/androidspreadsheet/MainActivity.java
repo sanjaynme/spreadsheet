b1c6426c008d8c839939726b06bed71d37d105c7
sanjaynme <sanju.l910bm@gmail.com>
2018-07-25 12:02:12 +0545

    added spreadsheet like design

---
+++ app/src/main/java/np/edu/nast/demoapp/androidspreadsheet/MainActivity.java
@@ -0,0 +1,13 @@
+package np.edu.nast.demoapp.androidspreadsheet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new TableMainLayout(this));
+    }
+}
---
+++ app/src/main/java/np/edu/nast/demoapp/androidspreadsheet/SampleObject.java
@@ -0,0 +1,64 @@
+package np.edu.nast.demoapp.androidspreadsheet;
+
+public class SampleObject {
+    String header1;
+    String header2;
+    String header3;
+    String header4;
+    String header5;
+    String header6;
+    String header7;
+    String header8;
+    String header9;
+    String header10;
+    String header11;
+    String header12;
+    String header13;
+    String header14;
+    String header15;
+    String header16;
+    String header17;
+    String header18;
+    String header19;
+    String header20;
+    String header21;
+    String header22;
+    String header23;
+    String header24;
+    String header25;
+
+    public SampleObject(String header1, String header2, String header3,
+                        String header4, String header5, String header6,
+                        String header7, String header8, String header9,
+                        String header10, String header11, String header12,
+                        String header13, String header14, String header15,
+                        String header16, String header17, String header18,
+                        String header19, String header20, String header21,
+                        String header22, String header23, String header24, String header25) {
+        this.header1 = header1;
+        this.header2 = header2;
+        this.header3 = header3;
+        this.header4 = header4;
+        this.header5 = header5;
+        this.header6 = header6;
+        this.header7 = header7;
+        this.header8 = header8;
+        this.header9 = header9;
+        this.header10 = header10;
+        this.header11 = header11;
+        this.header12 = header12;
+        this.header13 = header13;
+        this.header14 = header14;
+        this.header15 = header15;
+        this.header16 = header16;
+        this.header17 = header17;
+        this.header18 = header18;
+        this.header19 = header19;
+        this.header20 = header20;
+        this.header21 = header21;
+        this.header22 = header22;
+        this.header23 = header23;
+        this.header24 = header24;
+        this.header25 = header25;
+    }
+}
